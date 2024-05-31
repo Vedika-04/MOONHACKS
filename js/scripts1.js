2a0474db27ca7e3e66c002c124f27f71158641dc
Vedika Joshi <148654768+Vedika-04@users.noreply.github.com>
2024-05-31 07:50:27 +0530

    Add files via upload
    This folder contains all the JAVASCRIPT files used in website TechMsp.

---
+++ js/scripts1.js
@@ -0,0 +1,7 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+    window.location.href = '../pages/login.html'; // Redirect to the sign-in page
+});
+
+document.getElementById('signUpBtn').addEventListener('click', function() {
+    window.location.href = '../pages/register.html'; // Redirect to the sign-up page
+});
